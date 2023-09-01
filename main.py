917020788f50db036d19d82ec3cf71abc191a2a0
Collins Ezedike-egwom <62267326+collinsezedike@users.noreply.github.com>
2023-09-01 20:20:34 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
 import cv2
+import base64
 
 import numpy as np
 
 from PIL import Image
+from io import BytesIO
 from collections import Counter
 from sklearn.cluster import KMeans
 from werkzeug.utils import secure_filename
 
 from flask_bootstrap import Bootstrap5
 from flask import Flask, flash, redirect, render_template, request, url_for, session
-
-
 
 
 def extract_colors(img_array, num_colors=10):
@@ -40,7 +40,7 @@ def extract_colors(img_array, num_colors=10):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 10
-app.config["UPLOAD_EXTENSIONS"] = [".jpg"]
+app.config["VALID_EXTENSIONS"] = [".jpg"]
 Bootstrap5(app)
 
 
@@ -63,23 +63,23 @@ def recieve_image():
             filename = secure_filename(image.filename)
             if filename != "":
                 file_ext = os.path.splitext(filename)[1]
-                if file_ext not in app.config["UPLOAD_EXTENSIONS"]:
-                    flash("Image must be a .jpg type")
-                else:
+                if file_ext in app.config["VALID_EXTENSIONS"]:
                     pillow_image = Image.open(image)
                     image_array = np.array(pillow_image)
-                    session["colors"] = extract_colors(image_array)
-                    return redirect(url_for("display_palette"))
+                    colors = extract_colors(image_array)
+                    
+                    buffered = BytesIO()
+                    pillow_image.save(buffered, format="JPEG")
+                    img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+                    return render_template("colors.html", image=img_base64, colors=colors)
+                else:
+                    flash("Image must be a .jpg type")
             else:
                 flash("Invalid file")
         else:
             flash("This field is required!")
     return render_template("upload_image.html")
-
-
-@app.route("/color-palette/")
-def display_palette():
-    return render_template("colors.html")
 
 
 if __name__ == "__main__":
